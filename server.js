c30526affab120138fda3ba6080195452c38a71b
Jane Philipps <jane.philipps@gmail.com>
2015-07-17 17:50:39 -0700

    adding restful routes

--- server.js
+++ server.js
@@ -11,16 +11,49 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set up static assets
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
+
+// TEST DATA
+var cats = [
+    {name: "Rascal",
+     birthMonth: "February",
+     birthYear: "2012",
+     type: "tabby"},
+    {name: "GreyBear",
+     birthMonth: "August",
+     birthYear: "2014",
+     type: "siamese"},
+    {name: "Oliver",
+     birthMonth: "November",
+     birthYear: "2010",
+     type: "orange tabby"},
+  ];
 
 // ROUTES
 app.get('/', function (req, res) {
+  res.json(cats);
   console.log("Meow!");
+});
+
+app.get('/cats', function (req, res) {
+  res.send("Cats page!");
+});
+
+app.get('/cats/:id', function (req, res) {
+  res.send("Cat profile page!");
+});
+
+app.get('/cats/new', function (req, res) {
+  res.send("Create a cat profile!");
+});
+
+app.post('/cats', function (req, res) {
+  // post new cat data to MongoDB
 });
 
 var server = app.listen(process.env.PORT || 3000, function () {
     var host = server.address().address;
     var port = server.address().port;
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Meowspace app listening at http://%s:%s', host, port);
 });
